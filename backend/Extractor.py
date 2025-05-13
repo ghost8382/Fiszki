61e6b2211ba2e4853c39c82646bcbe9c2463d49e
ghost8382 <testdrive15@interia.pl>
2025-05-13 13:20:24 +0200

    Wersja beta

--- backend/Extractor.py
+++ backend/Extractor.py
@@ -5,11 +5,3 @@ def extract_text(file):
     return text
 
 
-def extract_text(file):
-    from PyPDF2 import PdfReader
-    reader = PdfReader(file)
-    text = "\n".join(page.extract_text() or "" for page in reader.pages)
-
-    # Dodajemy logowanie, żeby sprawdzić, czy tekst jest poprawnie wyciągany
-    print("Extracted Text:\n", text)
-    return text
